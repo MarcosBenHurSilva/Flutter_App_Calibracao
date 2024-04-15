265a7519d8f0501c8c454d3663420c5be2bcc2ec
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-04-14 21:11:39 -0300

    fix: :bug: Corrigi o arquivo 'main.dart'

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
-// No MaterialApp (main.dart), defina as rotas para cada tela:
-MaterialApp(
-  initialRoute: '/',
-  routes: {
-    '/': (context) => HomeScreen(),
-    '/calibration': (context) => CalibrationScreen(),
-    '/instrument_management': (context) => InstrumentManagementScreen(),
-  },
-);
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart'; // Importe a tela inicial do seu aplicativo
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App de Calibração Metrológica',
+      theme: ThemeData(
+        primaryColor: Colors.blue, // Defina as cores e estilos do tema
+        hintColor: Colors.orange,
+        fontFamily: 'Roboto', // Defina a fonte padrão
+        // Outras configurações de tema...
+      ),
+      initialRoute: '/', // Rota inicial do aplicativo
+      routes: {
+        '/': (context) =>
+            HomeScreen(), // Defina as rotas para as telas do aplicativo
+        // Adicione mais rotas aqui conforme necessário
+      },
+    );
+  }
+}
